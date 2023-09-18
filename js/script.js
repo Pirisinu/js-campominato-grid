393a0bf1936cd6bd7c1f7e79e79cad8a3562263b
Pirisinu <palazzialessandro2@gmail.com>
2023-09-18 16:16:11 +0200

    [ADD]RESET FUNCTION

--- js/script.js
+++ js/script.js
@@ -8,10 +8,12 @@
   - (3)Funzione generatore random di numeri
   - (4)Funzione coloratore di classe con parametro (pari o dispari)
   - (5)Funzione generatrice di div
+  - (6)Funzione reset
  */
 
 const squareContainer = document.querySelector('.container');
-
+const difficultyChoice = document.querySelector('.difficulty-choice');
+reset()
 //FUNZIONI GENERALI
 
 //INIT FUNCTION
@@ -20,7 +22,12 @@ function init(){
 }
 //BTN START FUNCTION
 function btnStart(){
+  //creiamo il bottone in html
+ const btn = document.createElement('button');
+ //ci scrivo sopra
+ btn.innerHTML= 'Start';
 
+ return btn;
 }
 //RANDOM NUM GENERATOR FUNCTION
 function randomNum(){
@@ -33,4 +40,11 @@ function evenControll(){
 //ELEMENT GENERATOR FUNCTION
 function createElement(){
 
+}
+//RESET FUNCTION
+function reset(){
+  squareContainer.innerHTML = '';
+  const initButton = btnStart();
+  squareContainer.append(initButton)
+  difficultyChoice.classList.remove('d-none');
 }
