b5085183b1ead430d18cd1a7794be7bc124696ef
Pirisinu <palazzialessandro2@gmail.com>
2023-09-18 15:58:08 +0200

    [ADD]JS POINT 1./2.

---
+++ js/script.js
@@ -0,0 +1,36 @@
+ /* 
+**Svolgimento**
+
+1. Richiamo i vari elementi dall'html 
+2. Mi vado a creare varie funzioni
+  - (1)Funzione di init() programma
+  - (2)Funzione bottone di avvio
+  - (3)Funzione generatore random di numeri
+  - (4)Funzione coloratore di classe con parametro (pari o dispari)
+  - (5)Funzione generatrice di div
+ */
+
+const squareContainer = document.querySelector('.container');
+
+//FUNZIONI GENERALI
+
+//INIT FUNCTION
+function init(){
+
+}
+//BTN START FUNCTION
+function btnStart(){
+
+}
+//RANDOM NUM GENERATOR FUNCTION
+function randomNum(){
+
+}
+//EVEN CONTROLL FUNCTION
+function evenControll(){
+
+}
+//ELEMENT GENERATOR FUNCTION
+function createElement(){
+
+}
